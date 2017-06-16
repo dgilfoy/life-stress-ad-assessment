b7eb29359538fe12bed743786017fe5dc0d7ef2e
Daniel Gilfoy <dgilfoy@gmail.com>
2017-06-16 10:58:55 -0700

    relative path issue fix attempt

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: './'
     },
     devServer: {
         hot: true,
